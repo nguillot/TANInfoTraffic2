4d0fa186b39f623bcfd8260c7a3cc6c90770c19a
nguillot <nguillot@ippon.fr>
2013-03-20 18:06:39 +0100

    initialisation du projet et du service principal

---
+++ app/scripts/services/infoTrafficService.js
@@ -0,0 +1,77 @@
+'use strict';
+
+angular.module('taninfotraffic2App')
+  .factory('infoTrafficService', function ($resource, TANInfoTrafficAdapter) {
+  // Service logic
+
+  var localSvc = $resource('fixtures/getInfoTraficTANTempsReel.json');
+
+  //private properties
+  var _infoTraffics = [];
+
+  // Public API here
+  return {
+    get: function() {
+        return _infoTraffics;
+    },
+
+    //service methods
+    // This method retrieves all info traffic data from the service and stores it in _infoTraffics
+    prepareInfoTraffics : function(callback){
+        var self = this;
+        localSvc.get(function (data) {
+            //TOOD check response status code, and set rawData only if its ok
+            var rawData = data.opendata.answer.data.ROOT.LISTE_INFOTRAFICS.INFOTRAFIC;
+            angular.forEach(rawData,function(item){
+                var translatedItem = TANInfoTrafficAdapter.translateInfoTraffic(item);
+                _infoTraffics.push(translatedItem);
+            });
+            //pour les tests dump(_infoTraffics);
+            if (angular.isFunction(callback)) callback(_infoTraffics);
+        });
+    }
+  };
+});
+
+/*
+ * Json adapter.
+*/
+angular.module('taninfotraffic2App').factory('TANInfoTrafficAdapter',function() {
+    var TANInfoTrafficAdapter = {
+        translateInfoTraffic : function(rawInfoTraffic) {
+            var infoTraffic = {};
+            infoTraffic.title = rawInfoTraffic.INTITULE;
+            infoTraffic.description = rawInfoTraffic.RESUME;
+            infoTraffic.startingDate = rawInfoTraffic.DATE_DEBUT + " " + rawInfoTraffic.HEURE_DEBUT;
+            infoTraffic.endingDate = rawInfoTraffic.DATE_FIN + " " + rawInfoTraffic.HEURE_FIN;
+            infoTraffic.routes = getRoutes(rawInfoTraffic.TRONCONS);
+            infoTraffic.isFinished = rawInfoTraffic.PERTURBATION_TERMINEE !== "0";
+
+            return infoTraffic;
+        }
+    };
+
+    var getRoutes = function(rawRoutes) {
+        var routes = [];
+        if(angular.isString(rawRoutes)) {
+            //rawRoutes = "[27/1/-/-];[28/1/-/-]";
+            var tmpRoutes = rawRoutes.split(";") || [""];
+            //tmpRoutes = ["[27/1/-/-]", "[28/1/-/-]"];
+            for (var i=0,len=tmpRoutes.length; i<len; i++) {
+                var tmp = tmpRoutes[i].split("/");
+                //tmp = ["[27", "1", "-", "-"];
+                if(angular.isArray(tmp) && tmp.length > 0) {
+                    tmp = tmp[0];
+                    //tmp = "[27";
+                    tmp = tmp.substring(1, tmp.length);
+                    //tmp = "27";
+                    routes.push(tmp);
+                }
+            }
+        }
+
+        return routes;
+    };
+
+    return TANInfoTrafficAdapter;
+});
---
+++ test/spec/services/infoTrafficService.js
@@ -0,0 +1,100 @@
+'use strict';
+
+describe('Service: infoTrafficService', function () {
+
+  // load the service's module
+  beforeEach(module('taninfotraffic2App'));
+
+/*  // instantiate service
+  var infoTrafficService;
+  beforeEach(inject(function (_infoTrafficService_) {
+    infoTrafficService = _infoTrafficService_;
+  }));
+
+  it('should do something', function () {
+    expect(!!infoTrafficService).toBe(true);
+  });
+*/
+
+  beforeEach(function(){
+    this.addMatchers({
+      toEqualData: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
+
+  // instantiate service
+  var infoTrafficService, $httpBackend;
+  beforeEach(inject(function($injector) {
+    infoTrafficService = $injector.get("infoTrafficService");
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', 'fixtures/getInfoTraficTANTempsReel.json').respond(
+                            {"opendata" :{
+                               "request" : "http://data.nantes.fr/api/getInfoTraficTANTempsReel/1.0/39W9VSNCSASEOGV/?output=json",
+                               "answer" :
+                                  {
+                                     "status" :
+                                        {
+                                           "@attributes" :
+                                              {
+                                                 "code" : "0",
+                                                 "message" : "OK"
+                                              }
+                                        },
+                                     "data" :
+                                        {
+                                           "ROOT" :
+                                              {
+                                                 "LISTE_INFOTRAFICS" :
+                                                    {
+                                                       "INFOTRAFIC" :
+                                                          [
+                                                             {
+                                                                "CODE" : "9",
+                                                                "LANGUE" : "0",
+                                                                "INTITULE" : "Travaux rue Esnoult des Châtelets à Nantes",
+                                                                "RESUME" : "En raison de travaux rue Esnoult des Châtelets à Nantes, le couloir de bus est neutralisé entre les rues Goudy et St Jacques, uniquement en direction de Greneraie, pour une durée de 15 mois.",
+                                                                "TEXTE_VOCAL" : "Travo rue ainou des chatelai a nante, larai sain jake est reporté a larai pirmile                .",
+                                                                "DATE_DEBUT" : "09/03/2012",
+                                                                "DATE_FIN" : "31/03/2013",
+                                                                "HEURE_DEBUT" : "13:36",
+                                                                "HEURE_FIN" : "21:30",
+                                                                "PERTURBATION_TERMINEE" : "0",
+                                                                "TRONCONS" : "[27/1/-/-];[28/1/-/-];[29/1/-/-];[36/1/-/-];[39/1/-/-];[94/2/-/-];[98/2/-/-];[68/1/-/-]"
+                                                             }
+                                                          ]
+                                                    }
+                                              }
+                                        }
+                                  }
+                            }
+                            }
+     );
+  }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should get all info traffic', function () {
+    $httpBackend.expectGET('fixtures/getInfoTraficTANTempsReel.json');
+    //$httpBackend.flush();
+
+    infoTrafficService.prepareInfoTraffics(function (data) {
+        //on doit placer le test dans la callback sinon il sera vérifier avant la fin de la méthode
+        //(asynchronisme du module $resource)
+        expect(infoTrafficService.get()).toEqualData([{"title" : "Travaux rue Esnoult des Châtelets à Nantes",
+                                                      "description" : "En raison de travaux rue Esnoult des Châtelets à Nantes, le couloir de bus est neutralisé entre les rues Goudy et St Jacques, uniquement en direction de Greneraie, pour une durée de 15 mois.",
+                                                      "startingDate" : "09/03/2012 13:36",
+                                                      "endingDate" : "31/03/2013 21:30",
+                                                      "isFinished" : false,
+                                                      "routes" : ["27","28","29","36","39","94","98","68"]
+                                                   }]);
+    });
+
+    $httpBackend.flush();
+  });
+});
